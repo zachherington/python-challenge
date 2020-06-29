9c5fe72f2d3663dd19eb0cb00378d2b42fe6595f
zachherington <zachherington@gmail.com>
2020-06-29 01:42:26 -0400

    PyPoll almost finished. 5/6 tasks complete. Still need to get the winner.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,7 +9,7 @@
     #   2.  List of candidates that received a vote
     #   3.  Total votes for each candidate
     #   4.  % of total votes for each candidate
-    #   5.  The greatest decrease in losses (date and amount) over the entire period
+    #   5.  Determine the election winner
     #   6.  Write the results to a text file. 
 
  
@@ -35,15 +35,15 @@ with open(datapath) as csvfile:
 
     # Create separate lists to store each category
     for row in reader:
-        voter_id.append((row[0]))
-        county.append((row[1]))
-        candidate.append((row[2]))
+        voter_id_all.append((row[0]))
+        county_all.append((row[1]))
+        candidate_all.append((row[2]))
 
 # 1. Number of votes
-    total_votes = len(voter_id)
+    total_votes = len(voter_id_all)
 
 # 2. Candidate List
-    cand_list = list(set(candidate))
+    cand_list = list(set(candidate_all))
     total_cand = len(cand_list)
     # Store each name for reference
     cand1 = cand_list[0]
@@ -51,25 +51,35 @@ with open(datapath) as csvfile:
     cand3 = cand_list[2]
     cand4 = cand_list[3]
     
-# 3. Total Votes by candidate
+# 3. Total Votes by candidate 
     cand1_votes = []
     cand2_votes = []
     cand3_votes = []
     cand4_votes = []
     
     for x in candidate_all:
-        if x = cand1:
-            cand1_votes.append
-        if x = cand2:
-            cand2_votes.append
-        if x = cand3:
-            cand3_votes.append
-        if x = cand4:
-            cand4_votes.append
+        if x == cand1:
+            cand1_votes.append(x)
+        if x == cand2:
+            cand2_votes.append(x)
+        if x == cand3:
+            cand3_votes.append(x)
+        if x == cand4:
+            cand4_votes.append(x)
     
     cand1_total = len(cand1_votes)
-    
+    cand2_total = len(cand2_votes)
+    cand3_total = len(cand3_votes)
+    cand4_total = len(cand4_votes)
 
+# 4. Percent Votes by candidate 
+    cand1_per = round((cand1_total / total_votes) *100 , 2)
+    cand2_per = round((cand2_total / total_votes) *100 , 2)
+    cand3_per = round((cand3_total / total_votes) *100 , 2)
+    cand4_per = round((cand4_total / total_votes) *100 , 2)
+
+# 5. Determine the election winner
+    winner = "TBD"
 
 # 6. Print the results and write to txt file
     print("----------------------------")
@@ -78,10 +88,10 @@ with open(datapath) as csvfile:
     print(f"Total Votes: {total_votes}")
     print(f"Total Candidates: {total_cand}")
     print("")
-    # print(f"{cand1}: {cand1_per}% with {cand1_sum} votes.")
-    # print(f"{cand2}: {cand1_per}% with {cand1_sum} votes.")
-    # print(f"{cand3}: {cand1_per}% with {cand1_sum} votes.")
-    # print(f"{cand4}: {cand1_per}% with {cand1_sum} votes.")
+    print(f"{cand1}: {cand1_per}% with {cand1_total} votes.")
+    print(f"{cand2}: {cand2_per}% with {cand2_total} votes.")
+    print(f"{cand3}: {cand3_per}% with {cand3_total} votes.")
+    print(f"{cand4}: {cand4_per}% with {cand4_total} votes.")
     print ("----------------------------")
-    # print(f"Winner: {win_candidate}")
+    print(f"Winner: {winner}")
     print ("----------------------------")
